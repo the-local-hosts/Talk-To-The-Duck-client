fe1f96766e718170054c1a72de2f3f030f28568d
hishamovski <hisham.boualam@gmail.com>
2019-06-26 14:27:20 -0400

    1- Added a username field to signup form 2- fix the closing forms ( it wasn't closed properly when we click on     sign-in, sign-up button) 3- Updated profile template 4- Added API methods (sign-in, sign-up and change password)   in auth/authApi.js
    pair programming: @hishamovski @kevinrosario

--- assets/scripts/auth/authApi.js
+++ assets/scripts/auth/authApi.js
@@ -1,1 +1,47 @@
 'use strict'
+
+const store = require('../store')
+const config = require('../config.js')
+
+const signUp = formData => {
+  return $.ajax({
+    url: config.apiUrl + 'sign-up',
+    method: 'POST',
+    data: formData
+  })
+}
+
+const signIn = formData => {
+  return $.ajax({
+    url: config.apiUrl + 'sign-in',
+    method: 'POST',
+    data: formData
+  })
+}
+
+const changePassword = formData => {
+  return $.ajax({
+    url: config.apiUrl + 'change-password',
+    method: 'PATCH',
+    data: formData,
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+const signOut = () => {
+  return $.ajax({
+    url: config.apiUrl + 'sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut
+}
--- assets/scripts/auth/authEvents.js
+++ assets/scripts/auth/authEvents.js
@@ -1,5 +1,7 @@
 'use strict'
 const ui = require('./authUi')
+const api = require('./authApi')
+const getFormFields = require('../../../lib/get-form-fields')
 
 const setLogIn = function () {
   ui.setLogin()
@@ -16,8 +18,37 @@ const setSignIn = function (event) {
   ui.setSignIn()
 }
 
+const onSignIn = function (event) {
+  event.preventDefault()
+  const formData = getFormFields(event.target)
+  console.log(formData)
+
+  api.signIn(formData)
+    .then(ui.onSignInSuccess)
+    .catch(ui.onSignInFailure)
+}
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  const formData = getFormFields(event.target)
+  console.log(formData)
+
+  api.signUp(formData)
+    .then(ui.onSignInSuccess)
+    .catch(ui.onSignInFailure)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  const formData = getFormFields(event.target)
+  console.log(formData)
+}
+
 module.exports = {
   setLogIn,
   setSignUp,
-  setSignIn
+  setSignIn,
+  onSignIn,
+  onSignUp,
+  onChangePassword
 }
