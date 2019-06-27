5d6c33a6eff0d93eac2009a2daad93144d364938
elizadavis <eelizadavis@gmail.com>
2019-06-27 11:12:34 -0400

    Add Function to Display Create New Post Form
    Added a way to show our 'new post' form while also being able to hide
    it on page load
    Pair programmed by @hollyklose and @elizadavis

--- assets/scripts/blogposts/ui.js
+++ assets/scripts/blogposts/ui.js
@@ -1,12 +1,19 @@
 'use strict'
 
 const indexTemplate = require('../templates/index.handlebars')
+const newBlogpostTemplate = require('../templates/new-blogpost.handlebars')
 
 const onIndexSuccess = function (responseData) {
   const indexHTML = indexTemplate({ posts: responseData.posts })
   $('.main-content').html(indexHTML)
 }
 
+const showCreateForm = function () {
+  const newBlogpostHtml = newBlogpostTemplate
+  $('.main-content').html(newBlogpostHtml())
+}
+
 module.exports = {
-  onIndexSuccess
+  onIndexSuccess,
+  showCreateForm
 }
