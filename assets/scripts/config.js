54e8c6ba38b3d1a3b853f6ef5ecf25c8e319f0fc
kevinrosario <kevin.rosario9@upr.edu>
2019-06-28 16:35:09 -0400

    Fixed production URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://pure-plains-50623.herokuapp.com/',
+  production: 'https://pure-plains-50623.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
